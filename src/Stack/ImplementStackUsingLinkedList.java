923a9fef8ebc4c4f52587f6f760def0467354956
Someshwar7462 <someshwargupta15@gmail.com>
2026-01-04 19:41:51 +0530

    Correct the display function

--- src/Stack/ImplementStackUsingLinkedList.java
+++ src/Stack/ImplementStackUsingLinkedList.java
@@ -43,28 +43,28 @@ class Stack1{
         else return false;
     }
 
-    void display(Node head){
+    void display(){
         Node temp=head;
-        if(temp==null) return;
-        display(temp.next);
-        System.out.println(temp.val);
-
+        while(temp!=null){
+            System.out.println(temp.val);
+            temp=temp.next;
+        }
     }
 }
 public class ImplementStackUsingLinkedList {
     public static void main(String[] args) {
-        Stack st=new Stack();
-        st.push(10);
-        st.push(20);
-        st.push(30);
-        st.push(40);
-        st.push(50);
-        st.display();
-        System.out.println(st.peek());
-        System.out.println(st.size());
-        st.pop();
-        st.pop();
-        System.out.println(st.size());
-        System.out.println(st.isEmpty());
+        Stack1 st1=new Stack1();
+        st1.push(10);
+        st1.push(20);
+        st1.push(30);
+        st1.push(40);
+        st1.push(50);
+        st1.display();
+        System.out.println(st1.peek());
+        System.out.println(st1.size);
+        st1.pop();
+        st1.pop();
+        System.out.println(st1.size);
+        System.out.println(st1.isEmpty());
     }
 }
